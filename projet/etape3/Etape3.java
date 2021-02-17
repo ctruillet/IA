2b9a47fbed616fc1dc435224e005ef07e19cadef
Clement Truillet <clement@ctruillet.eu>
2021-02-17 14:27:07 +0100

    Fin Etape3

--- projet/etape3/Etape3.java
+++ projet/etape3/Etape3.java
@@ -22,28 +22,32 @@ public class Etape3{
         GrapheDeLieux tg = GrapheDeLieux.loadGraph("../Data/town10.txt",true) ;
         UneSolution tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 10 villes de 0 a 9 : \n");
-        solverHC.hilClimbing(tsp,1000);
-        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1);
         solverHC.hilClimbing(tsp,10);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1000);
         
         System.out.println("======== Solver 2 pour 10 villes de 0 a 9 : \n");
-        solverTabou.tabou(tsp,1000); 
-        solverTabou.tabou(tsp,100); 
-        solverTabou.tabou(tsp,10);                              
+        solverTabou.tabou(tsp,1); 
+        solverTabou.tabou(tsp,10); 
+        solverTabou.tabou(tsp,100);
+        solverTabou.tabou(tsp,1000);                              
                                    
         // ///////////////////
         // cas 2 : 26 villes de 0 à 25
         tg = GrapheDeLieux.loadGraph("../Data/town30.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 26 villes de 0 a 25 : \n");
-        solverHC.hilClimbing(tsp,1000);
-        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1);
         solverHC.hilClimbing(tsp,10);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1000);
 
         System.out.println("======== Solver 2 pour 26 villes de 0 a 25 : \n");
-        solverTabou.tabou(tsp,1000); 
-        solverTabou.tabou(tsp,100); 
-        solverTabou.tabou(tsp,10);                          
+        solverTabou.tabou(tsp,1); 
+        solverTabou.tabou(tsp,10); 
+        solverTabou.tabou(tsp,100);
+        solverTabou.tabou(tsp,1000);                    
                                    
                                    
         // ///////////////////
@@ -51,14 +55,16 @@ public class Etape3{
         tg = GrapheDeLieux.loadGraph("../Data/town150.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 150 villes : \n");
-        solverHC.hilClimbing(tsp,1000);
-        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1);
         solverHC.hilClimbing(tsp,10);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1000);
 
         System.out.println("======== Solver 2 pour 150 villes : \n");
-        solverTabou.tabou(tsp,1000); 
-        solverTabou.tabou(tsp,100); 
-        solverTabou.tabou(tsp,10);                            
+        solverTabou.tabou(tsp,1); 
+        solverTabou.tabou(tsp,10); 
+        solverTabou.tabou(tsp,100);
+        solverTabou.tabou(tsp,1000);                           
                                    
                                    
         // ///////////////////
@@ -66,13 +72,15 @@ public class Etape3{
         tg = GrapheDeLieux.loadGraph("../Data/town1000.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 1000 villes : \n");
-        solverHC.hilClimbing(tsp,1000);
-        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1);
         solverHC.hilClimbing(tsp,10);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,1000);
 
         System.out.println("======== Solver 2 pour 1000 villes : \n");
-        solverTabou.tabou(tsp,1000); 
-        solverTabou.tabou(tsp,100); 
-        solverTabou.tabou(tsp,10);                               
+        solverTabou.tabou(tsp,1); 
+        solverTabou.tabou(tsp,10); 
+        solverTabou.tabou(tsp,100);
+        solverTabou.tabou(tsp,1000);                            
     }
 }
--- projet/etape4/Etape4.java
+++ projet/etape4/Etape4.java
@@ -7,53 +7,63 @@ import solvers.* ;
 
 public class Etape4 {
 
-   public static void main (String [] args) {
-   
+   public static void main (String [] args) {   
+        SolverCSP solverCSP;
         System.out.println("\n====== TEST de SolverCSP ====") ;
         System.out.println("=============================") ;
         
         // TEST 1 : town10.txt avec 3 couleurs
         GrapheDeLieux tg = GrapheDeLieux.loadGraph("../Data/town10.txt",true) ;
-		System.out.println("\nTest sur town10 avec 3 couleurs (on attend OK) :") ;
-        // choisir ici un algo et l'executer 
+             
+        System.out.println("\nTest sur town10 avec 3 couleurs (on attend OK) :") ;
+        solverCSP = new SolverCSP(tg,3);
+        solverCSP.solve();
 		
         // TEST 2 : town10.txt avec 2 couleurs
 		System.out.println("\nTest sur town10 avec 2 couleurs (on attend NOK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,2);
+        solverCSP.solve();
 		
         // TEST 3 : town10.txt avec 4 couleurs
 		System.out.println("\nTest sur town10 avec 4 couleurs (on attend OK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,4);
+        solverCSP.solve();
 
 
 		   
         // TEST 4 : flat20_3_0.col avec 4 couleurs
         tg = GrapheDeLieux.loadGraph("../Data/pb-etape1/flat20_3_0.col",false) ;
         System.out.println("Test sur flat20_3_0.col avec 4 couleurs (on attend OK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,4);
+        solverCSP.solve();
     
         // TEST 5 : flat20_3_0.col avec 3 couleurs
         System.out.println("Test sur flat20_3_0.col avec 3 couleurs (on attend OK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,3);
+        solverCSP.solve();
     
         // TEST 6 : flat20_3_0.col avec 2 couleurs
         System.out.println("Test sur flat20_3_0.col avec 2 couleurs (on attend NOK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,2);
+        solverCSP.solve();
 
 
     
         // TEST 7 : jean.col avec 10 couleurs
         tg = GrapheDeLieux.loadGraph("../Data/pb-etape1/jean.col",false) ;
         System.out.println("Test sur jean.col avec 10 couleurs (on attend OK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,10);
+        solverCSP.solve();
     
         // TEST 9 : jean.col avec 3 couleurs
         System.out.println("Test sur jean.col avec 3 couleurs (on attend NOK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,3);
+        solverCSP.solve();
     
         // TEST 8 : jean.col avec 9 couleurs
         System.out.println("Test sur jean.col avec 9 couleurs (on attend NOK) :") ;
-        // choisir ici un algo et l'executer 
+        solverCSP = new SolverCSP(tg,9);
+        solverCSP.solve();
 		
 		
 	}
