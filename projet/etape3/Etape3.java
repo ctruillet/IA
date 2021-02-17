410b9abc33e49cf409dffaa22de946f4a605be70
Clement Truillet <clement@ctruillet.eu>
2021-02-17 14:03:00 +0100

    Etape 3

--- projet/etape3/Etape3.java
+++ projet/etape3/Etape3.java
@@ -5,8 +5,7 @@ import solvers.* ;
 
 
 /** classe pour realiser l'etape 3 du projet (suite tache 2) */
-public class Etape3
-{
+public class Etape3{
 
     // tests
     // ////////////////////////////////////////////
@@ -23,9 +22,13 @@ public class Etape3
         GrapheDeLieux tg = GrapheDeLieux.loadGraph("../Data/town10.txt",true) ;
         UneSolution tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 10 villes de 0 a 9 : \n");
-        solverHC.hilClimbing(tsp);
+        solverHC.hilClimbing(tsp,1000);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,10);
         
         System.out.println("======== Solver 2 pour 10 villes de 0 a 9 : \n");
+        solverTabou.tabou(tsp,1000); 
+        solverTabou.tabou(tsp,100); 
         solverTabou.tabou(tsp,10);                              
                                    
         // ///////////////////
@@ -33,10 +36,14 @@ public class Etape3
         tg = GrapheDeLieux.loadGraph("../Data/town30.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 26 villes de 0 a 25 : \n");
-        solverHC.hilClimbing(tsp);
+        solverHC.hilClimbing(tsp,1000);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,10);
 
         System.out.println("======== Solver 2 pour 26 villes de 0 a 25 : \n");
-        solverTabou.tabou(tsp,10);                              
+        solverTabou.tabou(tsp,1000); 
+        solverTabou.tabou(tsp,100); 
+        solverTabou.tabou(tsp,10);                          
                                    
                                    
         // ///////////////////
@@ -44,9 +51,13 @@ public class Etape3
         tg = GrapheDeLieux.loadGraph("../Data/town150.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 150 villes : \n");
-        solverHC.hilClimbing(tsp);
+        solverHC.hilClimbing(tsp,1000);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,10);
 
         System.out.println("======== Solver 2 pour 150 villes : \n");
+        solverTabou.tabou(tsp,1000); 
+        solverTabou.tabou(tsp,100); 
         solverTabou.tabou(tsp,10);                            
                                    
                                    
@@ -55,9 +66,13 @@ public class Etape3
         tg = GrapheDeLieux.loadGraph("../Data/town1000.txt",true) ;
         tsp = new UneSolution(tg) ;
         System.out.println("======== Solver 1 pour 1000 villes : \n");
-        solverHC.hilClimbing(tsp);
+        solverHC.hilClimbing(tsp,1000);
+        solverHC.hilClimbing(tsp,100);
+        solverHC.hilClimbing(tsp,10);
 
         System.out.println("======== Solver 2 pour 1000 villes : \n");
-        solverTabou.tabou(tsp,10);                                
+        solverTabou.tabou(tsp,1000); 
+        solverTabou.tabou(tsp,100); 
+        solverTabou.tabou(tsp,10);                               
     }
 }
